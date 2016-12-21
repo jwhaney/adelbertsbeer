080d8a094eca6758852b0e584e23ba922970870e
geojwh <geojwh@gmail.com>
2016-12-21 16:49:37 -0600

    leaflet zoom home

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 //create map
-var map = L.map('map').setView([31.755, -94.444], 6);
+var map = L.map('map', {zoomControl: false}).setView([31.755, -94.444], 6);
 
 //load osm basemap tiles
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -7,6 +7,9 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //controls
+var zoomHome = L.Control.zoomHome();
+zoomHome.addTo(map);
+
 L.control.locate().addTo(map);
 
 var searchControl = L.esri.Geocoding.geosearch().addTo(map);
@@ -23,20 +26,15 @@ L.geoJson(states, {
 L.geoJson(counties, {
     style: function (feature) {
         switch (feature.properties.DIST) {
-            case 'Favorite Brands New Mexico': return {color: "#0099ff", fillOpacity: 0, weight: 2};
+            case 'Favorite Brands New Mexico': return {color: "#ff9900", fillOpacity: 0, weight: 2};
             case 'L&F Distributors LLC': return {color: "#ff3333", fillOpacity: 0, weight: 2};
             case 'Ben E. Keith': return {color: "#00cc66", fillOpacity: 0, weight: 2};
             case 'Jack Hilliard Distributing': return {color: "#996633", fillOpacity: 0, weight: 2};
             case 'Sons of John': return {color: "#9933ff", fillOpacity: 0, weight: 2};
-            case 'Crafty Connoisseurs Distributing': return {color: "#ffa31a", fillOpacity: 0, weight: 2};
+            case 'Crafty Connoisseurs Distributing': return {color: "#0066ff", fillOpacity: 0, weight: 2};
         }
     }
 }).addTo(map);
 
 //load adelberts.json and style
-var redMarker = L.AwesomeMarkers.icon({
-    icon: 'glass',
-    markerColor: 'red'
-});
-
-L.marker([-97.7199070, 30.3825210], {icon: redMarker}).addTo(map);
+L.geoJson(adelberts).addTo(map);
